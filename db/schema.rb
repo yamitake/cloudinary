1c72fe9352b7d9f7babcd6748b95630dc1811b68
jun takeno <take.yapr@gmail.com>
2015-07-29 14:59:20 +0900

    upload for localhost

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729053235) do
+ActiveRecord::Schema.define(version: 20150729053946) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "avatar"
   end
 
 end
